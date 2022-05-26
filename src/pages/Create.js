c91fc66263286eb8ca0664c57db2443449bf44e3
Jerin S <jerinmanjadiyil@gmail.com>
2022-05-26 15:44:15 +0530

    created title,image and description

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,27 +1,50 @@
 import { Button } from 'react-bootstrap'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { InputGroup, FormControl } from 'react-bootstrap'
+import {Form} from 'react-bootstrap'
 
 const Create = () => {
-
+  const [title,setTitle] = useState("");
+  const[desc,setDesc] = useState("");
     const navigate = useNavigate();
 
     const goBack = () =>{
         navigate(`/Users/${localStorage.getItem('userId')}`);
 
     }
+
+    
+   
+    const updateTitle = ev =>{
+        setTitle(ev.target.value); 
+    } 
+
+    const updateDesc =  ev =>{
+      setDesc(ev.target.value); 
+  } 
+
+    useEffect(() => {
+      console.log(title,desc);
+    }, [title,desc])
+    
+
   return (
     <div>
         <Button onClick={goBack} variant='primary' style={{float:"left"}}> Back </Button>
-        <InputGroup className="mb-3">
-    <FormControl
-      placeholder="Title"
-      aria-label="Title"
-      aria-describedby="basic-addon1"
-    />
-  </InputGroup>
-
+        <h1>{title}</h1>
+        <Form>
+  <Form.Group className="mb-3" controlId="Form.ControlInput1">
+    <Form.Control type="text" placeholder="Title" value={title} onChange={updateTitle} />
+  </Form.Group>
+  <Form.Group controlId="formFile" className="mb-3">
+    <Form.Label>Default file input example</Form.Label>
+    <Form.Control type="file" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="Form.ControlTextarea1">
+    <h1> {desc}</h1>
+    <Form.Control as="textarea" rows={3} onChange={updateDesc} value={desc} />
+  </Form.Group>
+</Form>
     </div>
   )
 }
