6f919a7a99db26697815817c30696aae9e480331
Jerin S <jerinmanjadiyil@gmail.com>
2022-05-30 23:33:17 +0530

    can upload images and titles to firestore

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,19 +2,39 @@ import { Button } from 'react-bootstrap'
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import {Form} from 'react-bootstrap'
+import { addDoc, collection } from 'firebase/firestore'
+import { auth,db, storage } from "../firebaseConfig"
+import { ref } from 'firebase/storage'
+import {v4} from 'uuid'
 
 const Create = () => {
   const [title,setTitle] = useState("");
   const[desc,setDesc] = useState("");
-    const navigate = useNavigate();
+  const [imageUpload, setImageUpload] = useState(null);
+  const [url,setUrl] = useState('')
+ // const [ imageList , setImageList ] = useState([]);
+  //const imageListRef = ref(storage,"images/")
+  const navigate = useNavigate();
 
     const goBack = () =>{
         navigate(`/Users/${localStorage.getItem('userId')}`);
 
     }
 
+    const uploadImage = ()=> {
+      if(imageUpload == null) return ;
+      ref(`/images/${imageUpload.name + v4()}`).put(imageUpload)
+      .on("state_changed", alert("success"), alert, () => {
+  
+        // Getting Download Link
+        storage.ref("images").child(imageUpload.name).getDownloadURL()
+          .then((url) => {
+            setUrl(url);
+          })
+      });
+  
+    }
     
-   
     const updateTitle = ev =>{
         setTitle(ev.target.value); 
     } 
@@ -26,25 +46,39 @@ const Create = () => {
     useEffect(() => {
       console.log(title,desc);
     }, [title,desc])
+
     
+    const postCollectionRef = collection(db,"posts");
+    const createPost = async() =>{
+      uploadImage();
+      await addDoc(postCollectionRef, {title,
+        imageUrl : url ,
+         desc ,
+         author:{ name: auth.currentUser.displayName , id : auth.currentUser.uid }
+        })
+        navigate('/');
+
+    }
 
   return (
     <div>
         <Button onClick={goBack} variant='primary' style={{float:"left"}}> Back </Button>
-        <h1>{title}</h1>
+        
         <Form>
   <Form.Group className="mb-3" controlId="Form.ControlInput1">
     <Form.Control type="text" placeholder="Title" value={title} onChange={updateTitle} />
   </Form.Group>
   <Form.Group controlId="formFile" className="mb-3">
     <Form.Label>Default file input example</Form.Label>
-    <Form.Control type="file" />
+    <Form.Control type="file" onChange={ (ev) => {setImageUpload(ev.target.files[0])}} />
   </Form.Group>
   <Form.Group className="mb-3" controlId="Form.ControlTextarea1">
-    <h1> {desc}</h1>
+    
     <Form.Control as="textarea" rows={3} onChange={updateDesc} value={desc} />
   </Form.Group>
 </Form>
+
+<Button variant='primary' type='button' onClick={createPost} >Submit</Button>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import banner from "../Assets/banner.png"
 import Bar from '../components/Bar'
 import CardPack from '../components/CardPack'
 import Image from 'react-bootstrap/Image'
+import { getDocs,collection } from 'firebase/firestore'
+import { db } from '../firebaseConfig'
 
 //import { Col, Container, Row } from 'react-bootstrap'
 
 
 function Home() {
   
+  // eslint-disable-next-line no-unused-vars
+  const [postLists, setPostList ] = useState([]);
+  const postCollectionRef = collection(db,"posts");
+  
+  const getPosts = async () =>{
+    const data = await getDocs(postCollectionRef);
+    setPostList(data.docs.map((doc)=> ({...doc.data(),id: doc.id})));
+  }
 
+  useEffect( () => {
+    getPosts();
+  })
   return (
     <div>
       <div className='d-flex justify-content-center mh-100' style={{height: 180}}>
@@ -18,6 +31,13 @@ function Home() {
       </div>
       <Bar />
       <CardPack />
+      <div className='cards'>
+        
+        {postLists.map((post)=> {
+          return  <div key={post.id} >{post.title}<br/>{post.desc}</div>
+        })
+      }
+      </div>
 
     
 
